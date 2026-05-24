e7df4e8562e6c80711130d25e10ed8c1a10cd00c
sinee0601 <sinee0601@gmail.com>
2026-05-24 11:39:27 +0900

    first commit

---
+++ src/main/java/com/travel/letsgospringboot/LetsgoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.travel.letsgospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsgoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsgoSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travel/letsgospringboot/LetsgoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.letsgospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsgoSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
