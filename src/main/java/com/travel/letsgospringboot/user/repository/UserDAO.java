a755ef878292a4a63ecbc3618b120e2d568f3d15
Seeun <happysarahkim2004@gmail.com>
2026-05-27 08:50:00 +0900

    UserDAO 테스트 코드 추가

--- src/main/java/com/travel/letsgospringboot/user/repository/UserDAO.java
+++ src/main/java/com/travel/letsgospringboot/user/repository/UserDAO.java
@@ -1,13 +1,15 @@
 package com.travel.letsgospringboot.user.repository;
 
 import com.travel.letsgospringboot.user.vo.UserVO;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserDAO {
     UserVO login(String userID, String password);
 
-    boolean signup(String userID, String email, String name, String password);
+    boolean signUp(String userID, String email, String name, String password);
 
-    boolean idcheck(String userID);
+    boolean idCheck(String userID);
 
     boolean updatePassword(String userID, String email, String newPassword);
 
