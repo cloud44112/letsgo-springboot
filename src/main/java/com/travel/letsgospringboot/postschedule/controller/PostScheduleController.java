71ab3a7fee699744ad34dfe6aef32280453a3587
Mskko <mskk0410@gmail.com>
2026-06-04 17:38:22 +0900

    feat : Controller 최초 HTML, 데이터코드 작성

--- src/main/java/com/travel/letsgospringboot/postschedule/controller/PostScheduleController.java
+++ src/main/java/com/travel/letsgospringboot/postschedule/controller/PostScheduleController.java
@@ -1,14 +1,25 @@
 package com.travel.letsgospringboot.postschedule.controller;
 
+import com.travel.letsgospringboot.postschedule.service.PostScheduleService;
+import com.travel.letsgospringboot.postschedule.vo.PostScheduleTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/postschedule")
+@RequiredArgsConstructor
 public class PostScheduleController {
 
+    private final PostScheduleService postScheduleService;
+
     @GetMapping("/list")
-    public String postScheduleList(){
+    public String postScheduleList(Model model) {
+        List<PostScheduleTO> list = postScheduleService.getPostScheduleListLatest();
+        model.addAttribute("list", list);
         return "postScheduleList";
     }
 }
