8fdd86eeeb9b20db3f3141cfbe53d6a722432763
sinee0601 <sinee0601@gmail.com>
2026-06-11 16:07:34 +0900

    feat: Exception Handling 패키지 추가

--- src/main/java/com/travel/letsgospringboot/exception/ScheduleAccessDeniedException.java
+++
@@ -1,8 +0,0 @@
-package com.travel.letsgospringboot.exception;
-
-
-public class ScheduleAccessDeniedException extends RuntimeException {
-    public ScheduleAccessDeniedException(String message) {
-        super(message);
-    }
-}
