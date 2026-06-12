76741744e12325b2ac5460da24339b3e69c707e5
Mskko <mskk0410@gmail.com>
2026-06-12 11:53:50 +0900

    fix : 어노테이션 명시 추가 - @ControllerAdvice에 annotaions = Controller.calss추가

--- src/main/java/com/travel/letsgospringboot/advice/RestExceptionHandling.java
+++ src/main/java/com/travel/letsgospringboot/advice/RestExceptionHandling.java
@@ -4,14 +4,16 @@ import com.travel.letsgospringboot.exception.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 import java.util.Map;
 import java.util.HashMap;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(annotations = RestController.class)
 public class RestExceptionHandling {
 
     private ResponseEntity<Map<String, Object>> buildErrorResponse(HttpStatus status, String message) {
